75f90201557c2316962eaddb96f4de3356567ce5
A-jabbar-tech <jabbarmalikfaujian786@gmail.com>
2025-07-25 12:12:33 +0500

    project 2 , setup + intro to class components , tutorial22

---
+++ Tutorials/react tuto 21-41/newsmokey/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello my First react class based componnents</h1>
+      </div>
+    )
+  }
+}
