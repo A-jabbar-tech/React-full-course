7b21224d06378abc8d3333e80f0fcd661bf66884
A-jabbar-tech <jabbarmalikfaujian786@gmail.com>
2025-07-27 20:40:07 +0500

    Understanding state in class based components , tutorial 25

--- Tutorials/react tuto 21-41/newsmokey/src/Componets/NewsItem/NewsItem.jsx
+++ Tutorials/react tuto 21-41/newsmokey/src/Componets/NewsItem/NewsItem.jsx
@@ -1,21 +1,22 @@
 import {React, Component } from "react";
 
 export class NewsItem extends Component {
+  
   render() {
 
-    let { title , description } = this.props;  // destructuring methood to unpack values from props to use directly 
+    let { title , description  , imageUrl , newsUrl } = this.props;  // destructuring methood to unpack values from props to use directly 
 
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail/" className="btn btn-sm btn-dark">
+              Read More
             </a>
           </div>
         </div>
